190881c100f214d0a9c0c1192f809a39274bdb00
mihika14 <mihikasaxena13@gmail.com>
2022-09-28 18:56:54 +0530

    final changes

--- src/components/AboutUs/Components/Introtext.jsx
+++ src/components/AboutUs/Components/Introtext.jsx
@@ -5,6 +5,7 @@ const Introtext = () => {
   return (
     <div className={classes.introText}>
         <div className={classes.introTextch}>
+          <br />
         Simmi Foundation envisions to develop a society based on legitimate <b> rights, equity, justice, honesty, social sensitivity</b> and <b> a culture of service</b> in which all are self-reliant.We look forward to <b>alleviate poverty</b>  and <b>enhance the dignty of poor</b> in the world by addressing basic, but neglected issues of poor. Education is one of the means through which we can acheive our goals.Women Empowerment is another mode of development, as women are an important section of our society and their empowerment can bring socio-economic and political progress of the country.
 
         </div>
